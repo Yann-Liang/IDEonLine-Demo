c35f0f9c19d9d1297a6364349237295fd27f3a89
Yann-Liang <liangyanxiangde@163.com>
2017-11-06 00:10:57 +0800

    上传项目

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# IDEonLine-Demo
-在线solidity编辑器，是否支持导入导出钱包文件示例
+# cloud-platform
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8081
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/views/index/index.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="">
+        <section>
+            <input type="file" id="uploadFile" @onchange="readLocalFile($event)"/>
+            <div><img src="" id="img"/></div>
+
+            <div><textarea id="fileContent" rows="30" cols="60"></textarea></div>
+        </section>
+    </div>
+</template>
+
+<script>
+    //import  from ''
+
+    export default {
+        //组件名
+        name: 'index',
+        //实例的数据对象
+        data() {
+            return {
+
+            }
+        },
+        //数组或对象，用于接收来自父组件的数据
+        props: {
+
+        },
+        //计算
+        computed: {
+
+        },
+        //方法
+        methods: {
+            readLocalFile(e){
+                console.log(e);
+                var localFile = document.getElementById("uploadFile").files[0];
+
+                var reader = new FileReader();
+                //Uncaught SyntaxError:Unexpected identifier
+                var content;
+                reader.onload = function(event) {
+                    content = event.target.result;
+                    //alert(content);
+
+                    document.getElementById("img").src = content;
+                    document.getElementById("fileContent").value = content;
+                }
+                reader.onerror = function(event) {
+                alert('error')
+                    //alert("File could not be read! Code " + event.target.error.code);
+                }
+                //reader.readAsText(localFile,"UTF-8");
+
+        content = reader.readAsDataURL(localFile,"UTF-8");
+            }
+        },
+        //生命周期函数
+        created() {
+
+        },
+        beforeMount() {
+
+        },
+        mounted() {
+
+        },
+        //监视
+        watch: {
+
+        },
+        //组件
+        components: {
+
+        },
+        //过滤器
+        filters:{
+
+        },
+        //自定义指令
+        directive:{
+
+        }
+    }
+</script>
+
+<!--
+	作者：liangyanxiangde@163.com
+	时间：2017-03-27
+	描述：统一使用less,局部样式
+-->
+<style lang="less" scoped>
+
+</style>
